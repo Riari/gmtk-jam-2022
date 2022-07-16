e0a61f68e74247885b9fe162cf6f7527d0195046
Rick Mann <3583774+Riari@users.noreply.github.com>
2022-07-16 18:14:43 +0100

    Finish ladder mechanism

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -12,6 +12,9 @@ public class MainCamera : MonoBehaviour
     [field: SerializeField]
     public float PanSpeed = 5.0f;
 
+    [field: SerializeField]
+    public float AutoPanSpeed = 25.0f;
+
     private const float MinSize = 3.0f;
     private const float MaxSize = 6.0f;
 
@@ -20,10 +23,14 @@ public class MainCamera : MonoBehaviour
 
     private Vector3 _pan = Vector3.zero;
 
+    readonly private Vector3 _defaultFocalPoint = new Vector3(0, 0, -10);
+    private Vector3 _focalPoint;
+
     void Start()
     {
         _camera = GetComponent<Camera>();
         _targetSize = _camera.orthographicSize;
+        _focalPoint = _defaultFocalPoint;
     }
 
     void Update()
@@ -33,7 +40,25 @@ public class MainCamera : MonoBehaviour
             _camera.orthographicSize = Mathf.MoveTowards(_camera.orthographicSize, _targetSize, SmoothSpeed * Time.deltaTime);
         }
 
+        if (_focalPoint != _defaultFocalPoint)
+        {
+            _camera.transform.position = Vector3.MoveTowards(_camera.transform.position, _focalPoint, AutoPanSpeed * Time.deltaTime);
+
+            if (_focalPoint == _camera.transform.position)
+            {
+                _focalPoint = _defaultFocalPoint;
+            }
+
+            return;
+        }
+
         _camera.transform.position += _pan * Time.deltaTime;
+    }
+
+    public void FocusOn(GameObject obj)
+    {
+        _focalPoint.x = obj.transform.position.x;
+        _focalPoint.y = obj.transform.position.y;
     }
 
     public void OnLeft(InputAction.CallbackContext context)
