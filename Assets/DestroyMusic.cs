3cd49cfcb901fc8a4aa21d383a2997d6daded329
GMWolf1988 <87359978+GMWolf1988@users.noreply.github.com>
2022-07-17 12:55:54 +0100

    Music

---
+++ Assets/DestroyMusic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("BackgroundMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
